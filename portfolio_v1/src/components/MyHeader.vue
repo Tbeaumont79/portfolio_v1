febeb6ea5922de8de539709af137ab77be2fb411
tbeaumont79 <tbeaumont79@icloud.com>
2023-10-30 18:58:55 +0100

    the header is now responsive on mobile + working on the responsive

--- portfolio_v1/src/components/MyHeader.vue
+++ portfolio_v1/src/components/MyHeader.vue
@@ -38,28 +38,30 @@
     >
       <div class="fixed inset-0 z-10" />
       <DialogPanel
-        class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10"
+        class="fixed inset-0 z-10 w-full bg-gray-900 sm:ring-1 sm:ring-white/10"
       >
-        <div class="flex items-center justify-between">
-          <button
-            type="button"
-            class="-m-2.5 rounded-md p-2.5 text-text"
-            @click="mobileMenuOpen = false"
-          >
-            <span class="sr-only">Close menu</span>
-            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div class="mt-6 flow-root">
+        <div class="w-full h-screen bg-background/95 flow-root">
+          <div class="relative">
+            <button
+              type="button"
+              class="rounded-md p-2.5 text-text"
+              @click="mobileMenuOpen = false"
+            >
+              <span class="sr-only right-0">Close menu</span>
+              <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
           <div class="-my-6 divide-y divide-gray-500/25">
-            <div class="space-y-2 py-6">
-              <router-link
-                v-for="item in navigation"
-                :key="item.name"
-                :to="item.href"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-text hover:bg-gray-800"
-                >{{ item.name }}</router-link
-              >
+            <div class="p-6">
+              <ul>
+                <router-link
+                  v-for="item in navigation"
+                  :key="item.name"
+                  :to="item.href"
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-text hover:text-accent"
+                  ><li>{{ item.name }}</li></router-link
+                >
+              </ul>
             </div>
           </div>
         </div>
