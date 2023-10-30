48d7661bf53992d6d6ad57519967d28d5f19b3fd
tbeaumont79 <tbeaumont79@icloud.com>
2023-10-30 19:21:48 +0100

    another fix on the home page

--- portfolio_v1/src/App.vue
+++ portfolio_v1/src/App.vue
@@ -3,7 +3,7 @@ import MyHeader from './components/MyHeader.vue'
 </script>
 
 <template>
-  <div class="w-full bg-background">
+  <div class="w-full bg-background h-screen p-4">
     <div class="container mx-auto">
       <MyHeader />
       <router-view></router-view>
--- portfolio_v1/src/components/Home.vue
+++ portfolio_v1/src/components/Home.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="flex justify-evenly w-full h-screen">
+  <div
+    class="flex sm:flex-row sm:items-start sm:mt-10 flex-col-reverse justify-evenly w-full"
+  >
     <img
       src="../assets/thib.jpg"
-      class="w-1/2 h-1/2 object-cover hidden sm:block"
+      class="sm:w-1/2 sm:h-1/2 object-cover py-5"
       alt=""
     />
-    <div class="flex-col">
-      <h1 class="text-4xl text-text">Hey I'm Thibault, a FrontEnd Developer</h1>
-      <h1 class="text-2xl text-text">
+    <div class="flex flex-col gap-y-10 h-60 sm:h-96 justify-center">
+      <h1 class="text-2xl sm:text-4xl text-text">
+        Hey I'm Thibault, a FrontEnd Developer
+      </h1>
+      <h1 class="text-accent font-bold">
         I love to craft websites, with great design
       </h1>
     </div>
